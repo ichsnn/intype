c680068890adb6a7597a951dc1665a246fa984c1
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-02-02 09:15:08 +0700

    add hotkeys

--- src/views/student/ComposeGrammar/components/CheckButton.tsx
+++ src/views/student/ComposeGrammar/components/CheckButton.tsx
@@ -1,7 +1,17 @@
+import { Tooltip } from 'react-tooltip';
+
 export default function CheckButton() {
   return (
-    <button className="min-w-[160px] px-6 py-3 gap-10 rounded-full border text-2xl border-slate-400 bg-slate-200 text-slate-400 font-bold" type="button" disabled>
-      Cek
-    </button>
-  )
+    <>
+      <button
+        id="checkbutton"
+        className="min-w-[160px] px-6 py-3 gap-10 rounded-full border text-2xl disabled:border-slate-400 disabled:bg-slate-200 disabled:text-slate-400 font-bold enabled:bg-green-500 enabled:text-white enabled:border-green-900 cursor-pointer"
+        type="button"
+        disabled={false}
+      >
+        Cek
+      </button>
+      <Tooltip anchorId="checkbutton" content="Enter" />
+    </>
+  );
 }
--- src/views/student/ComposeGrammar/components/SkipButton.tsx
+++ src/views/student/ComposeGrammar/components/SkipButton.tsx
@@ -1,9 +1,16 @@
-import cn from "classnames"
+import { Tooltip } from 'react-tooltip';
 
 export default function SkipButton() {
   return (
-    <button className="min-w-[160px] px-6 py-3 gap-10 rounded-full border text-2xl border-slate-400 bg-white text-slate-400 font-bold" type="button" disabled>
-      Lewati
-    </button>
-  )
+    <>
+      <button
+        id="skipbutton"
+        className="min-w-[160px] px-6 py-3 gap-10 rounded-full border text-2xl border-slate-400 bg-white text-slate-400 font-bold cursor-pointer"
+        type="button"
+      >
+        Lewati
+      </button>
+      <Tooltip anchorId="skipbutton" content="Alt + S" />
+    </>
+  );
 }
