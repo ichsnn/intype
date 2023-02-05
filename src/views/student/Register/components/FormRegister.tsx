b0bcf59bdf2c0d0ff8a3bab2ef71fb77f4845414
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-02-05 12:45:26 +0700

    remove unused

--- src/views/student/Register/components/FormRegister.tsx
+++ src/views/student/Register/components/FormRegister.tsx
@@ -11,7 +11,7 @@ export default function FormRegister() {
     register,
     handleSubmit,
     watch,
-    formState: { isDirty, isValid, dirtyFields },
+    formState: { isDirty, isValid },
   } = useForm();
 
   const [loading, setLoading] = useState(false);
