e55a375b5319b844f4b1c9a5588c4857659fab31
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-02-05 16:10:52 +0700

    update password

--- src/views/student/Settings/components/Password/index.tsx
+++ src/views/student/Settings/components/Password/index.tsx
@@ -2,20 +2,42 @@ import { Form, FormInput } from '@/components/Form';
 import Header from '../Header';
 import Button from '@/components/Button';
 import style from './Password.module.css';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
+import { apiPost } from '@/service/api';
+import { toast } from 'react-toastify';
 
 export default function StudentSettingsPassword() {
-  const { register } = useForm();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { isValid, isLoading },
+  } = useForm();
+
+  const onSubmit = async (values: FieldValues) => {
+    try {
+      const token = localStorage.getItem('access_token') as string;
+      const response = await apiPost('/student/update/password', {
+        data: values,
+        token,
+      });
+      toast(response.message, { type: 'success' });
+    } catch (error) {
+      const { response } = error as any;
+      toast(response.data.message, { type: 'error' });
+    }
+  };
+
   return (
     <section>
       <Header label="Password" />
-      <Form className="flex flex-col gap-9">
+      <Form className="flex flex-col gap-9" onSubmit={handleSubmit(onSubmit)}>
         <div className={style.form_group}>
           <FormInput
             type={'password'}
             label="Password Lama"
             id="password"
-            {...register('password')}
+            {...register('oldPassword')}
           />
         </div>
         <div className={style.form_group}>
@@ -23,7 +45,7 @@ export default function StudentSettingsPassword() {
             type={'password'}
             label="Password Baru"
             id="newpassword"
-            {...register('newpassword')}
+            {...register('newPassword')}
           />
         </div>
         <div className={style.form_group}>
@@ -31,10 +53,17 @@ export default function StudentSettingsPassword() {
             type={'password'}
             label="Konfirmasi Password"
             id="confirmpassword"
-            {...register('confirmpassword')}
+            {...register('confirmPassword', {
+              validate: (value) => value === getValues('newPassword'),
+            })}
           />
         </div>
-        <Button label="Ubah Password" primary />
+        <Button
+          type="submit"
+          label="Ubah Password"
+          primary
+          disabled={!isValid}
+        />
       </Form>
     </section>
   );
